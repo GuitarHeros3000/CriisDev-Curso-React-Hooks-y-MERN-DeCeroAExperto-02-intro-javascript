3d050898a114837c4268172999fa5afee62fd06e
Cristian David Ramírez de la Rosa <crisronald3000@outlook.com>
2024-09-12 14:47:26 -0600

    [ADD] Creación de 01-const-let.js y index.js.

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+
+// Variables y constantes
+
+const name = 'Cristian';
+const lastName = 'de la Rosa';
+
+let valueDice = 5;
+valueDice = 4;
+
+console.log(name, lastName, valueDice);
+
+if (true) {
+    const name = 'CriisDev';
+    console.log(name);
+}
+
+console.log(valueDice);
